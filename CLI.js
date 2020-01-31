9db0823352adb1955824d89dc25125da613c2ac5
D V <voronov.dmitry@gmail.com>
2020-01-31 06:42:12 -0500

    3rd Commit

--- CLI.js
+++ CLI.js
@@ -39,12 +39,45 @@ inquirer.
 
 // get reponses and filter them 
 .then (function(response){
+    console.log(response.title)
+    switch (response.title){
+        //If engineer, ask for git hub address
+        case "Engineer":
+        inquirer.
+        prompt([
+            {
+            type:"input",
+            name: "gitHub",
+            message:"What is your gitHub Address?"
+            },
+        ])
 
+    break;
+
+    case "Manager":
+        inquirer.
+        prompt([
+            {
+                type:"type",
+                name:"officeNum",
+                message:"What is your office number?"
+            },
+            
+        ])
+        case "Intern":
+            inquirer.prompt([
+                {
+                    type:"input",
+                    name:"school",
+                    Message:"What school did you got to?"
+                }
+            ])
+    }
 });
 
-    getRole = () =>{
+    
 
-    }
+    
 
     /**1. Create array/object with emplyees
      * 2. Got through array and attach names data obtained from the inquirer to the right class
